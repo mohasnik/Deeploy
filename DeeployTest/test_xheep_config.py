5964500250c7779ad12a8448bafd1a0b8628947f
mohasnik <mhr.nikkhah@gmail.com>
2026-09-19 16:51:44 +0200

    runner is updated, working on test_platforms.py now

---
+++ DeeployTest/test_xheep_config.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2025 ETH Zurich and University of Bologna
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Test list for the XHEEP platform.
+# Each entry is a relative path under DeeployTest/Tests/.
+
+KERNEL_TESTS = [
+    "Kernels/FP32/Add/Regular",
+    "Kernels/Integer/Add/Regular",
+]
